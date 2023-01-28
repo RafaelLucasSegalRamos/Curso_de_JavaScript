bf2ed41ebf47cef562c98c2ab7041efd25ed67e7
Rafael <94694500+RafaelLucasSegalRamos@users.noreply.github.com>
2023-01-28 10:34:10 -0300

    11 Class

---
+++ Modulo_D/Aula011/ex08.js
@@ -0,0 +1,8 @@
+var vel = 12
+
+if (vel > 60)//Condição Simples
+{
+    console.log(`Você recebeu uma multa, pois seu carro estava acima de ${vel}Km/H`)
+}
+
+console.log(`Sempre tente dirigir com cuidado, verifique o cinto de segurança!`)
---
+++ Modulo_D/Aula011/ex09.js
@@ -0,0 +1,10 @@
+var pais = "Brasil"
+
+if (pais == "Brasil")
+{
+    console.log('Você é Brasileiro| BR BR BR BR')
+}
+else
+{
+    console.log(`Você mora no(a) ${pais}... Gringo bobão`)
+}
