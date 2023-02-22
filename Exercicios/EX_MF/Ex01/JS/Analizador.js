3a1c6d7fd894426d86d709993c3d74f4891df3a0
Rafael <94694500+RafaelLucasSegalRamos@users.noreply.github.com>
2023-02-22 19:53:52 -0300

    end of lesson 7

--- Exercicios/EX_MF/Ex01/JS/Analizador.js
+++ Exercicios/EX_MF/Ex01/JS/Analizador.js
@@ -4,9 +4,8 @@ var final = document.getElementById('res')
 
 var numero = document.getElementById('num')
 
-var c = 0
 var list = []
-
+c = 0
 
 
 function adiciona()
@@ -34,20 +33,66 @@ function adiciona()
 
 function valida(valor)
 {
-    pos = 0
-    for (pos in list)
+    for (let pos in list)
     {
-        if (list[pos] == valor){
+        if (list[pos] == valor){   
             return true
         }
-        else
-        {
-            return false
-        }
     }
+    return false
+    
 }
 
 function finalizar()
 {
+    final.innerHTML = ""
+    final.innerHTML += `<p>Ao todo, temos ${list.length} número(os)</p>` 
 
+    let mn = maior(list)
+    let men = menor(list)
+
+    final.innerHTML += `<p>O maior valor informado foi ${mn}</p>`
+    final.innerHTML += `<p>O menor valor informado foi ${men}</p>`
+
+    let soma = somar(list)
+
+    final.innerHTML += `<p>Somando todos os valores teremos ${soma}</p>`
+
+    final.innerHTML += `<p>E a média aritimética desses valores é ${soma/c}</p>`
+}
+
+function maior(vetor)
+{
+    let maior = vetor[0]
+    for (let i in vetor)
+    {
+        if (vetor[i] > maior)
+        {
+            maior = vetor[i]
+        }
+    }
+    return maior
+}
+
+function menor(vetor)
+{
+    let menor = vetor[0]
+    for (let i in vetor)
+    {
+        if (vetor[i] < menor)
+        {
+            menor = vetor[i]
+        }
+    }
+    return menor
+}
+
+function somar(vetor)
+{
+    let somaf = 0
+    for (let i in vetor)
+    {
+        somaf += vetor[i]
+    }
+    return somaf
 }
